801c49fdf3161d507ae67f89d62f1c61f02f1c6d
Adam Ali <aa3.adamali@gmail.com>
2024-08-13 03:51:43 -0700

    Setup dev env

---
+++ Netflix-Clone/Netflix-Clone/Netflix_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Netflix_CloneApp.swift
+//  Netflix-Clone
+//
+//  Created by Adam Ali on 8/13/24.
+//
+
+import SwiftUI
+
+@main
+struct Netflix_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
