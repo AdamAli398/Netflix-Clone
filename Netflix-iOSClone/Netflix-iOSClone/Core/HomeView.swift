b5e9ce5640bc0b649faa8dfd209bb513d8174cca
Adam Ali <aa3.adamali@gmail.com>
2024-08-14 23:57:10 -0700

    Setup HomeView with header icons

---
+++ Netflix-iOSClone/Netflix-iOSClone/Core/HomeView.swift
@@ -0,0 +1,41 @@
+//
+//  HomeView.swift
+//  Netflix-iOSClone
+//
+//  Created by Adam Ali on 8/14/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        ZStack {
+            Color.netflixBlack.ignoresSafeArea()
+            
+            VStack (spacing: 0) {
+                HStack(spacing: 0) {
+                    Text("For You")
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .font(.title)
+                    
+                    HStack(spacing: 16) {
+                        Image(systemName: "tv.badge.wifi")
+                            .onTapGesture {
+                                // TODO: add tap gesture
+                            }
+                        Image(systemName: "magnifyingglass")
+                            .onTapGesture {
+                                // TODO: add tap gesture
+                            }
+                    }
+                    .font(.title2)
+                }
+            }
+        }
+        .foregroundStyle(.netflixWhite)
+    }
+}
+
+#Preview {
+    HomeView()
+}
---
+++ Netflix-iOSClone/Netflix-iOSClone/Netflix_iOSCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Netflix_iOSCloneApp.swift
+//  Netflix-iOSClone
+//
+//  Created by Adam Ali on 8/14/24.
+//
+
+import SwiftUI
+
+@main
+struct Netflix_iOSCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
