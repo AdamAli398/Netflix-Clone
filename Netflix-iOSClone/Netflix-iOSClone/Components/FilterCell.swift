69eb540c365ce356b3e1c54c7ff5b38e4f1131e4
Adam Ali <aa3.adamali@gmail.com>
2024-08-15 01:12:09 -0700

    Add FilterCell component

---
+++ Netflix-iOSClone/Netflix-iOSClone/Components/FilterCell.swift
@@ -0,0 +1,51 @@
+//
+//  FilterCell.swift
+//  Netflix-iOSClone
+//
+//  Created by Adam Ali on 8/15/24.
+//
+
+import SwiftUI
+
+struct FilterCell: View {
+    var title: String = "Categories"
+    var isDropdown: Bool = true
+    var isSelected: Bool = false
+    
+    var body: some View {
+        HStack(spacing: 4) {
+            Text(title)
+            
+            if isDropdown {
+                Image(systemName: "chevron.down")
+            }
+        }
+        .padding(.horizontal, 16)
+        .padding(.vertical, 8)
+        .background(
+            ZStack {
+                Capsule(style: .circular)
+                    .stroke(lineWidth: 1)
+
+                if isSelected {
+                    Capsule(style: .circular)
+                        .fill(.netflixDarkGrey)
+                }
+            }
+        )
+        .foregroundColor(.netflixLightGrey)
+    }
+}
+
+#Preview {
+    ZStack {
+        Color.black.ignoresSafeArea()
+        
+        VStack() {
+            FilterCell()
+            FilterCell(isSelected: true)
+            FilterCell(isDropdown: false)
+            FilterCell(isDropdown: false, isSelected: true)
+        }
+    }
+}
